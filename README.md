23f12a83c598bd6ab04d3d4e73c9c32b1a42ed21
Jim Nguyen <jimnguyen220@gmail.com>
2021-01-23 20:16:05 -0600

    continued work on functions, updated index.html and started README

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # weatherAPI
+The goal of this project is to create a weather app using a weather API. When the page is loaded, you see a search bar.  Type in the city and that will append a button underneath a search bar.  When you type in another city, it adds that city to the list of previously searched cities.  When you click on the button for a previous result, it will display the current weather with a 5 day forecast.
+
+-------------------
+
+screenshots go here
+
+--------------------
+
+
+## Resources:
+
+https://openweathermap.org/
+
+https://momentjs.com/
+
+https://getbootstrap.com
--- java.js
+++ java.js
@@ -1,34 +1,51 @@
 
-var cities = ["Minneapolis", "Seattle"];
-var cityValue = $("#city-search").val().trim();
+var cities = [];
+var currentDay = moment().format('dddd, MMMM Do YYYY');
 
 function renderButtons() {
     $("#cities-list").empty();
 
     for (var i=0; i < cities.length; i++) {
         var newButton = $("<button>");
+        newButton.addClass('city-button');
         newButton.attr("data-city", cities[i]);
         newButton.text(cities[i]);
         $("#cities-list").append(newButton);
     }
 }
 
-renderButtons();
+
 
 function cityWeather() {
     var cityValue = $("#city-search").val().trim();
-    var APIKey = "87378d54c0188556c3571925d4983352"
-    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?"+"q="+ cityValue + "&appid=" + APIKey;
+    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?"+"q="+ cityValue +"&cnt=5"+ "&appid=87378d54c0188556c3571925d4983352";
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
-        console.log(response);
+        // console.log(response +" cityWeather function");
     })
 
-  
+  cities.push(cityValue);
+  renderButtons();
+    $('#current-city-weather').text(cityValue+" on "+ currentDay);
 };
 
+function displayWeather(){
 
+    var cityValue = $(this).attr('data-city')
+    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q="+ cityValue +"&cnt=5"+"&appid=87378d54c0188556c3571925d4983352";
+
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+        console.log(queryURL + " displayWeather");
+    })
+};
+
+$(document).on('click', ".city-button", displayWeather);
 $("#search-button").on('click', cityWeather);
