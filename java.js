3557895e860c561499b9feab16b3b9a9dcbbba96
Jim Nguyen <jimnguyen220@gmail.com>
2021-01-24 22:37:21 -0600

    existing buttons update information

--- java.js
+++ java.js
@@ -1,4 +1,3 @@
-
 var cities = [];
 var currentDay = moment().format('dddd, MMMM Do YYYY');
 
@@ -80,6 +79,7 @@ function cityWeather() {
   renderButtons();
 };
 
+
 function displayWeather(){
 
     var cityValue = $(this).attr('data-city')
@@ -90,8 +90,45 @@ function displayWeather(){
         url: queryURL,
         method: "GET"
     }).then(function(response){
-        // console.log(response);
-        // console.log(queryURL + " displayWeather");
+        $('#current-city-weather').text(cityValue);
+        $('#current-time').text(currentDay);
+        console.log(response);
+
+        var forecast = response.list[0].weather[0].description;
+        $('#current-city-condition').html((forecast).toUpperCase());
+
+        var tempK = response.list[0].main.temp;
+        var tempF = Math.round((tempK - 273.15) * 1.80 + 32);
+        $('#current-city-temp').html(tempF + "&deg;F")
+
+        var humidity = response.list[0].main.humidity + "%"
+        $('#current-city-humitidy').text(humidity);
+
+        var wind = response.list[0].wind.speed + " mph";
+        $("#current-city-wind").text(wind);
+
+        var lat = response.city.coord.lat;
+        var lon = response.city.coord.lon;
+
+        var newURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=87378d54c0188556c3571925d4983352";
+        console.log(newURL);
+        $.ajax({
+            url: newURL,
+            method: "GET"
+        }).then(function(data){
+            var uvIndex = data.value;
+            $('#current-city-uv').text(uvIndex);
+
+            uvIndex = parseInt(uvIndex);
+            if (uvIndex <= 1.99) {
+                $('#current-city-uv').attr('class', 'favorable');
+            } else if (uvIndex >= 2 && uvIndex <= 5.99) {
+                $('#current-city-uv').attr('class', 'moderate');
+            } else {
+                $('#current-city-uv').attr('class', 'severe');
+            }
+        })
+
     })
 };
 
