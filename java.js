2cd3920b6768d99732f71dcca3337920534c0e7e
Jim Nguyen <jimnguyen220@gmail.com>
2021-01-27 16:39:01 -0600

    updated humidity forecast

--- java.js
+++ java.js
@@ -2,16 +2,19 @@ var cities = [];
 var currentDay = moment().format('dddd, MMMM Do YYYY');
 $('#current-time').text(currentDay);
 
-var dayPlusOne = moment().add(1, 'days').format("MM/DD/YY");
-$('#calendar-plus-one').text(dayPlusOne);
-var dayPlusTwo = moment().add(2, 'days').format("MM/DD/YY");
-$('#calendar-plus-two').text(dayPlusTwo);
-var dayPlusThree = moment().add(3, 'days').format("MM/DD/YY");
-$('#calendar-plus-three').text(dayPlusThree);
-var dayPlusFour = moment().add(4, 'days').format("MM/DD/YY");
-$('#calendar-plus-four').text(dayPlusFour);
-var dayPlusFive = moment().add(5, 'days').format("MM/DD/YY");
-$('#calendar-plus-five').text(dayPlusFive);
+function forecastDate(){
+    var dayPlusOne = moment().add(1, 'days').format("MM/DD/YY");
+    $('#calendar-plus-one').text(dayPlusOne);
+    var dayPlusTwo = moment().add(2, 'days').format("MM/DD/YY");
+    $('#calendar-plus-two').text(dayPlusTwo);
+    var dayPlusThree = moment().add(3, 'days').format("MM/DD/YY");
+    $('#calendar-plus-three').text(dayPlusThree);
+    var dayPlusFour = moment().add(4, 'days').format("MM/DD/YY");
+    $('#calendar-plus-four').text(dayPlusFour);
+    var dayPlusFive = moment().add(5, 'days').format("MM/DD/YY");
+    $('#calendar-plus-five').text(dayPlusFive);
+}
+
 
 
 function renderButtons() {
@@ -119,7 +122,7 @@ function displayWeather(){
         var forecastIcon = response.list[0].weather[0].icon;
         var newImage = $('<img>')
         var iconURL = "https://openweathermap.org/img/wn/" + forecastIcon + "@2x.png";
-        
+
         $('#current-city-condition').html((forecast).toUpperCase());
         newImage.attr('src', iconURL);
         $('#current-city-condition').prepend(newImage);
@@ -175,6 +178,8 @@ function forecastDisplay (cityValue) {
     $('#calendar-plus-three').empty();
     $('#calendar-plus-four').empty();
     $('#calendar-plus-five').empty();
+    
+    forecastDate();
 
     $.ajax({
         url: queryURL,
@@ -186,50 +191,55 @@ function forecastDisplay (cityValue) {
         var iconURL = "https://openweathermap.org/img/wn/" + forecastIconOne + "@2x.png";
         newImage.attr('src', iconURL);
         $('#calendar-plus-one').append(newImage);
-
         var tempKOne = response.list[1].main.temp;
         var tempFOne = Math.round((tempKOne - 273.15) * 1.80 + 32);
         $('#one-temp').html(tempFOne + "&deg;F")
+        var humidityOne = response.list[1].main.humidity + "%"
+        $('#one-humidity').text(humidityOne);
 
         var forecastIconTwo = response.list[2].weather[0].icon;
         var newImage = $('<img>')
         var iconURL = "https://openweathermap.org/img/wn/" + forecastIconTwo + "@2x.png";
         newImage.attr('src', iconURL);
         $('#calendar-plus-two').append(newImage);
-
         var tempKTwo = response.list[2].main.temp;
         var tempFTwo = Math.round((tempKTwo - 273.15) * 1.80 + 32);
         $('#two-temp').html(tempFTwo + "&deg;F")
+        var humidityTwo = response.list[2].main.humidity + "%"
+        $('#two-humidity').text(humidityTwo);
 
         var forecastIconThree = response.list[3].weather[0].icon;
         var newImage = $('<img>')
         var iconURL = "https://openweathermap.org/img/wn/" + forecastIconThree + "@2x.png";
         newImage.attr('src', iconURL);
         $('#calendar-plus-three').append(newImage);
-
         var tempKThree = response.list[3].main.temp;
         var tempFThree = Math.round((tempKThree - 273.15) * 1.80 + 32);
         $('#three-temp').html(tempFThree + "&deg;F")
+        var humidityThree = response.list[3].main.humidity + "%"
+        $('#three-humidity').text(humidityThree);
 
         var forecastIconFour = response.list[4].weather[0].icon;
         var newImage = $('<img>')
         var iconURL = "https://openweathermap.org/img/wn/" + forecastIconFour + "@2x.png";
         newImage.attr('src', iconURL);
         $('#calendar-plus-four').append(newImage);
-
         var tempKFour = response.list[4].main.temp;
         var tempFFour = Math.round((tempKFour - 273.15) * 1.80 + 32);
         $('#four-temp').html(tempFFour + "&deg;F")
+        var humidityFour = response.list[4].main.humidity + "%"
+        $('#four-humidity').text(humidityFour);
 
         var forecastIconFive = response.list[5].weather[0].icon;
         var newImage = $('<img>')
         var iconURL = "https://openweathermap.org/img/wn/" + forecastIconFive + "@2x.png";
         newImage.attr('src', iconURL);
         $('#calendar-plus-five').append(newImage);
-
         var tempKFive = response.list[5].main.temp;
         var tempFFive = Math.round((tempKFive - 273.15) * 1.80 + 32);
         $('#five-temp').html(tempFFive + "&deg;F")
+        var humidityFive = response.list[5].main.humidity + "%"
+        $('#five-humidity').text(humidityFive);
 
     })
 };
